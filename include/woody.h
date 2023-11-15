9b9a24541bf1998f1ea6007ec1ce51a361aade67
riblanc <richard.blc25@gmail.com>
2023-11-15 19:30:46 +0100

    ADD woody header file:
    Adding a basic woody header file containing default __EXEC_NAME__ if not defined -> when compiling manually I guess, but who would do that? I have a beautiful Makefile >:(

---
+++ include/woody.h
@@ -0,0 +1,13 @@
+//
+// Created by riblanc on 11/15/23.
+//
+
+#ifndef WW_PACKER_WOODY_H
+
+# ifndef __EXEC_NAME__
+#  define __EXEC_NAME__ "woody"
+# endif //__EXEC_NAME__
+
+#define WW_PACKER_WOODY_H
+
+#endif //WW_PACKER_WOODY_H

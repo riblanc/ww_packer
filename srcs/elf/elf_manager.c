8c0ef6340e28b3176fdcfc00b87e5685811ec258
riblanc <richard.blc25@gmail.com>
2023-11-28 15:46:33 +0100

    REWORK main structure definitions:
    Adding a new struct "t_file", storing everything about a file:
    the map region, the filename, the file stat and the file descriptor.
    
    Renaming program_header and section_header for ph_table and sh_table, + adding pht_size and sht_size on t_elf_info.

--- srcs/elf/elf_manager.c
+++ srcs/elf/elf_manager.c
@@ -8,6 +8,7 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/mman.h>
 
 #include "elf/elf_manager.h"
 #include "debug.h"
@@ -18,9 +19,13 @@ void	open_elf_cleaner(t_elf_error *eerror) {
 
 	DEBUG_LOG("cleaning elf, eerror ("BOLD"code: "RED"%d"RESET")", eerror->code);
 
-	if (eerror->elf->fd != -1) {
-		DEBUG_LOG("File opened, closing it ("BOLD"fd: "RED"%d"RESET")", eerror->elf->fd);
-		close(eerror->elf->fd);
+	if (eerror->elf->file.fd != -1) {
+		DEBUG_LOG(
+			"File opened, closing it ("BOLD"fd: "RED"%d"RESET")",
+			eerror->elf->file.fd
+		);
+		close(eerror->elf->file.fd);
+		eerror->elf->file.fd = -1;
 	}
 	return ;
 }
@@ -29,31 +34,50 @@ int	open_elf_file(t_elf_info *elf, const char *filename) {
 	t_elf_error eerror __attribute__((cleanup(open_elf_cleaner))) = {
 		.error = false,
 		.code = 0,
-		.filename = filename,
-		.elf = elf
+		.elf = elf,
 	};
 
+	elf->file.filename = filename;
+
 	ERRNO_PROTECT(
-		(elf->fd = open(filename, O_RDONLY | O_NONBLOCK)),
+		(elf->file.fd = open(filename, O_RDONLY | O_NONBLOCK)),
 		&eerror
 	);
 
 	ERRNO_PROTECT(
-		syscall(SYS_fstat, elf->fd, &elf->stat),
+		syscall(SYS_fstat, elf->file.fd, &elf->file.stat),
 		&eerror
 	);
 
 	ECODE_PROTECT(
-		S_ISDIR(elf->stat.st_mode),
+		S_ISDIR(elf->file.stat.st_mode),
 		EISDIR,
 		&eerror
 	);
 
 	CUSTOM_PROTECT(
-		S_ISFIFO(elf->stat.st_mode),
+		S_ISFIFO(elf->file.stat.st_mode),
 		eerror,
 		"Cannot read from a pipe"
 	);
+
+	CUSTOM_PROTECT(
+		S_ISSOCK(elf->file.stat.st_mode),
+		eerror,
+		"Cannot read from a socket"
+	);
+
+	ERRNO_PROTECT(
+		(elf->file.map = mmap(NULL, elf->file.stat.st_size, PROT_READ, MAP_PRIVATE, elf->file.fd, 0)),
+		&eerror
+	);
+
+	/* cf. man mmap:
+	 *   After the mmap() call has returned, the file descriptor, fd,
+	 *   can be closed immediately without invalidating the mapping.
+	*/
+	close(elf->file.fd);
+	elf->file.fd = -1;
 
 	return eerror.error ? EXIT_FAILURE : EXIT_SUCCESS;
 }
